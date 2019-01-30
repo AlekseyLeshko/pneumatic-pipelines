a39bab8edf5f7f72b613ff02ef2d5024ceb1ba71
AlekseyLeshko <alekseyleshko@gmail.com>
2019-01-30 21:06:54 +0300

    Rework

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const addItem = require('./helpers/add-item')
 const addArray = require('./helpers/add-array')
 const addString = require('./helpers/add-string')
 
+const addImage = addString('image')
+
 const step = {
   addArtifact: addItem('artifacts'),
   addArtifacts: addArray('artifacts'),
@@ -9,8 +11,12 @@ const step = {
   addCaches: addArray('caches'),
   addScript: addItem('script'),
   addScripts: addArray('script'),
-  addImage: addString('image'),
+  addImage,
   addDeployment: addString('deployment'),
+  addName: addString('name'),
 }
 
-module.exports = { step }
+module.exports = {
+  addImage,
+  step,
+}
