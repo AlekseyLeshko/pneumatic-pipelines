b3485b7c86761eb57064e8f620b808f59c19cecc
AlekseyLeshko <alekseyleshko@gmail.com>
2019-02-02 16:23:05 +0300

    Rework test

--- src/__tests__/generate.unit.test.js
+++ src/__tests__/generate.unit.test.js
@@ -1,39 +1,32 @@
 const fs = require('fs')
 const generate = require('../generate')
 
+const pipelines = {
+  image: 'node:11.0.0',
+  pipelines: { default: {} },
+}
+
 describe('generate', () => {
-  beforeEach(() => {
-    fs.writeFileSync = jest.fn()
-  })
+  beforeEach(() => fs.writeFileSync = jest.fn())
 
   afterEach(() => {
+    expect(fs.writeFileSync).toHaveBeenCalled()
     fs.writeFileSync.mockReset()
   })
 
   test('should create yaml file', () => {
-    const pipelines = {
-      image: 'node:11.0.0',
-      pipelines: { default: {} },
-    }
-
     generate(pipelines)
 
-    expect(fs.writeFileSync).toHaveBeenCalled()
     const params = fs.writeFileSync.mock.calls[0]
     expect(params[0]).toEqual('bitbucket-pipelines.yml')
     expect(typeof params[1]).toEqual('string')
   })
 
   test('should create yaml file with name', () => {
-    const pipelines = {
-      image: 'node:11.0.0',
-      pipelines: { default: {} },
-    }
     const fileName = '.travis.yml'
 
     generate(pipelines, fileName)
 
-    expect(fs.writeFileSync).toHaveBeenCalled()
     const params = fs.writeFileSync.mock.calls[0]
     expect(params[0]).toEqual(fileName)
     expect(typeof params[1]).toEqual('string')
