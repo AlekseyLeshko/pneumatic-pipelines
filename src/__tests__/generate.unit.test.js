a8b9566a39378a6fa9023fe425fedac76ae706a5
AlekseyLeshko <alekseyleshko@gmail.com>
2019-02-02 16:20:13 +0300

    Add test

--- src/__tests__/generate.unit.test.js
+++ src/__tests__/generate.unit.test.js
@@ -23,4 +23,19 @@ describe('generate', () => {
     expect(params[0]).toEqual('bitbucket-pipelines.yml')
     expect(typeof params[1]).toEqual('string')
   })
+
+  test('should create yaml file with name', () => {
+    const pipelines = {
+      image: 'node:11.0.0',
+      pipelines: { default: {} },
+    }
+    const fileName = '.travis.yml'
+
+    generate(pipelines, fileName)
+
+    expect(fs.writeFileSync).toHaveBeenCalled()
+    const params = fs.writeFileSync.mock.calls[0]
+    expect(params[0]).toEqual(fileName)
+    expect(typeof params[1]).toEqual('string')
+  })
 })
