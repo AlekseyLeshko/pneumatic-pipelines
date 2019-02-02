df443dfce2350ffb06aff737eca4bb178f03e2f4
AlekseyLeshko <alekseyleshko@gmail.com>
2019-02-02 15:35:10 +0300

    Refactoring of method

--- src/generate.js
+++ src/generate.js
@@ -2,8 +2,7 @@ const fs = require('fs')
 const yaml = require('yaml')
 const { strOptions } = require('yaml/schema')
 
-module.exports = (pipelines) => {
-  strOptions.fold.lineWidth = 0
-  const str = yaml.stringify(pipelines)
-  fs.writeFileSync('bitbucket-pipelines.yml', str)
-}
+strOptions.fold.lineWidth = 0
+
+module.exports = (pipelines, fileName = 'bitbucket-pipelines.yml') =>
+  fs.writeFileSync(fileName, yaml.stringify(pipelines))
