c090ef44837338bbc8bcd5a93a330c13ce11c602
AlekseyLeshko <alekseyleshko@gmail.com>
2019-02-06 22:46:03 +0300

    Add prestep

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const step = require('./step')
 const addParallel = require('./add-parallel')
 const addString = require('./helpers/add-string')
+const prestep = require('./prestep')
 
 module.exports = {
   image: addString('image')(),
   addParallel,
   step,
+  prestep,
   addDefinitions: (obj) => ({
     definitions: obj,
   }),
