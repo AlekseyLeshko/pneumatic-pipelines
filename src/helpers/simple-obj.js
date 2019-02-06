ee8364ec2f8a694ee7bb8538774509ba8b44a789
AlekseyLeshko <alekseyleshko@gmail.com>
2019-02-06 23:17:12 +0300

    Add helper

---
+++ src/helpers/simple-obj.js
@@ -0,0 +1,1 @@
+module.exports = (propName) => (obj) => ({ [propName]: obj })
--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ const step = require('./step')
 const addParallel = require('./add-parallel')
 const addString = require('./helpers/add-string')
 const prestep = require('./prestep')
+const simpleObj = require('./helpers/simple-obj')
 
 module.exports = {
   image: addString('image')(),
   addParallel,
   step,
   prestep,
-  addDefinitions: (obj) => ({
-    definitions: obj,
-  }),
-  addPipelines: (obj) => ({
-    pipelines: obj,
-  }),
+  addDefinitions: simpleObj('definitions'),
+  addPipelines: simpleObj('pipelines'),
 }
